be71c5d128596c7aac76553f58e7125a35090196
Luca Baldesi <luca.baldesi@unitn.it>
2017-11-20 14:28:49 +0100

    add libsrtp2 submodule

--- autogen.sh
+++ autogen.sh
@@ -5,4 +5,11 @@ test -z "$srcdir" && srcdir=.
 
 mkdir -p m4
 
-autoreconf --verbose --force --install || exit 1
+## Building dependencies
+git submodule init $srcdir/Libs/libsrtp
+git submodule update $srcdir/Libs/libsrtp
+DIR=$(pwd) && cd $srcdir/Libs/libsrtp/ && ./configure --prefix=$DIR/$srcdir/Libs/libsrtp/; cd $DIR
+make -C $srcdir/Libs/libsrtp
+##
+
+DIR=$(pwd) && cd $srcdir && autoreconf --verbose --force --install || exit 1; cd $DIR
