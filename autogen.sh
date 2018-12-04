cdde20e1acc069b9317df44a61deff8ea8875edc
Luca Baldesi <baldo.plus@gmail.com>
2018-12-04 11:30:51 +0100

    force static libsrtp2

--- autogen.sh
+++ autogen.sh
@@ -10,6 +10,7 @@ git submodule init $srcdir/Libs/libsrtp
 git submodule update $srcdir/Libs/libsrtp
 DIR=$(pwd) && cd $srcdir/Libs/libsrtp/ && ./configure --prefix=$DIR/$srcdir/Libs/libsrtp/; cd $DIR
 make -C $srcdir/Libs/libsrtp
+ln -s $(pwd)/Libs/libsrtp/include $srcdir/Libs/libsrtp/srtp2
 ##
 
 DIR=$(pwd) && cd $srcdir && autoreconf --verbose --force --install || exit 1; cd $DIR
